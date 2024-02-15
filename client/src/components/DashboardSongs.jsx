e740befba0dab346c657b7de0949f96091e38cf8
Vishnudev2002 <vhu.gpt143@gmail.com>
2024-02-15 18:51:19 +0530

    change commit

--- client/src/components/DashboardSongs.jsx
+++ client/src/components/DashboardSongs.jsx
@@ -22,7 +22,7 @@ const DashboardSongs = () => {
         });
       });
     }
-  }, [allSongs]);
+  }, []);
   return (
     <div className="w-full p-4 flex items-center justify-center flex-col">
       <div className="w-full flex justify-center items-center gap-20">
